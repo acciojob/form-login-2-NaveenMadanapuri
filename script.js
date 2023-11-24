0b3188da56c72645576a5779521f200b0e2b0dd6
NaveenMadanapuri <139567313+NaveenMadanapuri@users.noreply.github.com>
2023-11-24 18:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 //your JS code here. If required.
-function submitform(){
-	const firstName = document.getElementById('FN').value;
-    const lastName = document.getElementById('LN').value;
-    const phoneNumber = document.getElementById('fno').value;
-    const emailID = document.getElementById('ei').value;
-	
-	const alertMessage = "First Name: " + firstName + "\n" +
+function submitForm() {
+    // Get form values
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const email = document.getElementById('email').value;
+
+    // Display alert with form information
+    const alertMessage = "First Name: " + firstName + "\n" +
                          "Last Name: " + lastName + "\n" +
                          "Phone Number: " + phoneNumber + "\n" +
-                         "Email ID: " + emailID;
+                         "Email ID: " + email;
 
     alert(alertMessage);
 }
